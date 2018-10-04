c2c3d80e888c0c1e460ceb174eeb9f38cf50b9a7
Vitalii <nash.hobby@gmail.com>
2018-10-04 23:22:29 +0300

    задание 1 праравка 1

--- lesson 1/js/script.js
+++ lesson 1/js/script.js
@@ -1,23 +1,25 @@
+"use strict";
+
 let money = +prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let mandatoryExpense = prompt("Введите обязательную статью расходов в этом месяце",""),
-    howMuchExpense = prompt("Во сколько обойдется?", "");
-//получаем непредвиденные траты и сумму трат
-let expenses = {
-        mandatoryExpense: howMuchExpense
-    };
-//получаем масив доп. доходов
-let additionalIncome = [];
-
+    howMuchExpense = prompt("Во сколько обойдется?", ""),
+    mandatoryExpenseTwo = prompt("Введите обязательную статью расходов в этом месяце",""),
+    howMuchExpenseTwo = prompt("Во сколько обойдется?", "");
 let appData = {
     budget: money,
     timeData: time,
-    consumption: expenses,
-    optionalExpenses: "",
+    expenses: {
+        mandatoryExpense: howMuchExpense,
+        mandatoryExpenseTwo: howMuchExpenseTwo,
+    },
+    optionalExpenses: {},
+    additionalIncome: [],
     additionalIncome: "",
     savings: false
 };
+
 // выводим бюджет на день, без учёта расходов
 alert(money / 30);
 
