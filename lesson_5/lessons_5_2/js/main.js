fc9fe9f61a6a8f8495079ed8fc79ae15b78109d8
Vitalii <nash.hobby@gmail.com>
2018-10-13 23:34:05 +0300

    Задание 5 - подзадание 2

---
+++ lesson_5/lessons_5_2/js/main.js
@@ -0,0 +1,23 @@
+let startСalculating = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    btn = document.getElementsByTagName("button"),
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncomeLabel = document.querySelector('.choose-income-label'),
+    checksavings = document.querySelector('.checksavings'),
+    chooseCum = document.querySelector('.choose-sum'),
+    hoosePercent = document.querySelector('.hoose-percent'),
+    year = document.querySelector('.year input'),
+    month = document.querySelector('.month input'),
+    day = document.querySelector('.day input');
+console.log(chooseCum)
+    
+
+    
+
