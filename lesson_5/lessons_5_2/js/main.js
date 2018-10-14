9f711a2047548ba491448c085f647045fc052642
Vitalii <nash.hobby@gmail.com>
2018-10-14 09:33:14 +0300

    ЗАдание 5 - подзадание 2 - v_2

--- lesson_5/lessons_5_2/js/main.js
+++ lesson_5/lessons_5_2/js/main.js
@@ -1,13 +1,15 @@
 let startСalculating = document.getElementById('start'),
-    budgetValue = document.getElementsByClassName('budget-value'),
-    daybudgetValue = document.getElementsByClassName('daybudget-value'),
-    levelValue = document.getElementsByClassName('level-value'),
-    expensesValue = document.getElementsByClassName('expenses-value'),
-    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
-    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
-    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
     expensesItem = document.getElementsByClassName('expenses-item'),
-    btn = document.getElementsByTagName("button"),
+    expensesItemBtn = document.getElementsByTagName("button")[0],
+    optionalexpensesBtn = document.getElementsByTagName("button")[1],
+    countBudgetBtn = document.getElementsByTagName("button")[2],
     optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
     chooseIncomeLabel = document.querySelector('.choose-income-label'),
     checksavings = document.querySelector('.checksavings'),
@@ -16,7 +18,7 @@ let startСalculating = document.getElementById('start'),
     year = document.querySelector('.year input'),
     month = document.querySelector('.month input'),
     day = document.querySelector('.day input');
-console.log(chooseCum)
+console.log(optionalexpensesBtn)
     
 
     
