ceefe6d728093fcf3f116ae9d605c8d40907a686
Vitalii <nash.hobby@gmail.com>
2018-10-18 22:18:00 +0300

    Задание 8 на проверку

--- lesson_7/js/script.js
+++ lesson_7/js/script.js
@@ -34,4 +34,59 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+
+    //таймер 
+
+    let deadline = '2018-10-20';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            if (hours < 10) {
+                hours = "0" + hours;
+            } 
+            if (t <= 0) {
+                hours = "00";
+                minutes = "00";
+                seconds = "00";
+            }
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+        
+    }
+   
+    function setClock (id, endTime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+            
+        }
+        
+    }
+    setClock('timer', deadline);
 });
