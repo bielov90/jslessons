3f1890562784929a24d969e73681eddc1dc6f563
Vitalii <nash.hobby@gmail.com>
2018-11-01 10:36:41 +0200

    Диплом проверка

--- Diplom/window/js/script.js
+++ Diplom/window/js/script.js
@@ -1,22 +1,32 @@
 window.addEventListener('DOMContentLoaded', function(){
 
     'use strict';
-    
+    //Modal popup_engineer
     let headerBtnWrapBlock = document.querySelector('.header_btn_wrap_block'),
-        popup = document.querySelector('.popup'),
-        close = document.querySelector('.popup_close');
+        popupEngineer = document.querySelector('.popup_engineer'),
+        close1 = document.getElementsByClassName('popup_close')[1];
     
     
     headerBtnWrapBlock.addEventListener('click', function(){
-        popup.style.display = 'block';
-        this.classList.add('popup_engineer');
+        popupEngineer.style.display = 'block';
     });
-    close.addEventListener('click', function() {
-        popup.style.display = 'none';
-        headerBtnWrapBlock.classList.remove('popup_engineer');
+    close1.addEventListener('click', function() {
+        popupEngineer.style.display = 'none';
     });
     
+    //Modal popup
+    let phoneLink = document.getElementsByClassName('phone_link'),
+        popup = document.querySelector('.popup');
+        close = document.getElementsByClassName('popup_close')[0];
+        for (let i = 0; i < phoneLink.length; i++){
 
-
-
+            phoneLink[i].addEventListener('click', function(){
+                popup.style.display = 'block';
+                this.classList.add('popup_engineer');
+            });
+            close.addEventListener('click', function() {
+                popup.style.display = 'none';
+                phoneLink[i].classList.remove('popup_engineer');
+            });
+        }
 });
