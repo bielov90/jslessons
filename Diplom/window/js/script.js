b0ab8f22a332295259d8f4947f931951b4e1f810
Vitalii <nash.hobby@gmail.com>
2018-11-01 19:09:08 +0200

    Диплом проверка

--- Diplom/window/js/script.js
+++ Diplom/window/js/script.js
@@ -2,41 +2,44 @@ window.addEventListener('DOMContentLoaded', function(){
 
     'use strict';
     //Modal popup_engineer
-    let headerBtnWrapBlock = document.querySelector('.header_btn_wrap_block'),
+    let popupEngineerBtn = document.querySelector('.popup_engineer_btn'),
         popupEngineer = document.querySelector('.popup_engineer'),
         close1 = document.getElementsByClassName('popup_close')[1];
     
     
-    headerBtnWrapBlock.addEventListener('click', function(){
+        popupEngineerBtn.addEventListener('click', function(){
         popupEngineer.style.display = 'block';
     });
     close1.addEventListener('click', function() {
         popupEngineer.style.display = 'none';
     });
     
-    // popupEngineer.addEventListener('click', function() {
-    //     popupEngineer.style.display = 'none';
-    // });
+    popupEngineer.addEventListener('click', function(event) {
+        if (event.target === popupEngineer)
+        popupEngineer.style.display = 'none';
+    });
     //Modal popup
     let phoneLink = document.getElementsByClassName('phone_link'),
-        popup = document.querySelector('.popup');
+        popup = document.querySelector('.popup'),
         close = document.getElementsByClassName('popup_close')[0];
         for (let i = 0; i < phoneLink.length; i++){
 
             phoneLink[i].addEventListener('click', function(){
                 popup.style.display = 'block';
-                this.classList.add('popup_engineer');
-                
             });
-            setTimeout(function() {
-                popup.style.display = 'block';
-              }, 60000);
+         
             close.addEventListener('click', function() {
                 popup.style.display = 'none';
-                phoneLink[i].classList.remove('popup_engineer');
+            });
+            popup.addEventListener('click', function(event) {
+                if (event.target === popup)
+                popup.style.display = 'none';
             });
             
         }
+        setTimeout(function() {
+            popup.style.display = 'block';
+          }, 60000);
 
     // Timer
     let deadline = '2018-11-03';
