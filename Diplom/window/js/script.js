0fb629ea539c99f1f0f48ea0107250540e18be3f
Vitalii <nash.hobby@gmail.com>
2018-11-04 21:23:11 +0200

    Дипломное задание

--- Diplom/window/js/script.js
+++ Diplom/window/js/script.js
@@ -369,13 +369,38 @@ setClock('timer', deadLine);
                 let json = JSON.stringify(obj);
             request.send(json);
         });
-}
-postData(formData)
-    .then(() => (statusMessage.innerHTML = message.loading))
-    .then(() => (statusMessage.innerHTML = message.success))
-    .catch(() => (statusMessage.innerHTML = message.failure))
-    .then(clearInputs);
-});
-      
+    }
+    postData(formData)
+        .then(() => (statusMessage.innerHTML = message.loading))
+        .then(() => (statusMessage.innerHTML = message.success))
+        .catch(() => (statusMessage.innerHTML = message.failure))
+        .then(clearInputs);
+    });
+        
+    // Fotos - img
+    let img = document.querySelectorAll(".photo"),
+        pictures = document.querySelector(".pictures"),
+        bigPhoto = document.createElement("div");
 
+        function imgContent(a) {
+            pictures.insertBefore(bigPhoto, img[a]);
+            bigPhoto.classList.add("big_photo");
+        }
+        imgContent(0);
+
+    img.forEach(function(photo, index) {
+        photo.addEventListener("click", function(event) {
+            event.preventDefault();
+            bigPhoto.innerHTML = `<img src='img/our_works/big_img/${index + 1}.png'>`;
+            bigPhoto.classList.add("big_photo_img");
+            bigPhoto.style.display = "block";
+        });
+    });
+
+    bigPhoto.addEventListener("click", function(event) {
+        if (event.target.classList.contains("big_photo")) {
+            bigPhoto.classList.remove("big_photo_img");
+            bigPhoto.style.display = "none";
+        }
+    });
 });
